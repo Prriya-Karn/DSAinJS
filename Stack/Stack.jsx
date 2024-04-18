f4ae2431bf05510fd4795fb60018ca06c2dc6d46
Priya Karn <priya.karn.rn51@gmail.com>
2024-04-18 21:35:37 +0530

    Stack start

---
+++ Stack/Stack.jsx
@@ -0,0 +1,21 @@
+
+//---------------------- Stack start-------------------------------------
+
+// push operation :- push the element on the top of the stack
+
+const pushElement = (ele)=>{
+    var max = 5;
+    var stack = [];
+    if(stack.length<=max){
+        for(var i=0;i<stack.length;i++)
+        {
+            stack[i] = ele;
+            stack.length+=1;
+        }
+    }else{
+        return "can't add the element"
+    }
+    return stack;
+}
+
+console.log(pushElement(10));
